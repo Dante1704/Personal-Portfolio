fbeafaaf2e53c84c5f0ff37593c405ed38c00807
Dante <dante.nkaddarian@gmail.com>
2023-02-16 16:13:08 -0300

    agregado proyecto app countries

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import ContactForm from './ContactForm';
 const Contact = () => {
   return (
     <section id='contact' className='flex flex-col gap-10 max-w-2xl mx-auto px-2 pb-20'>
-      <h2 className='header-contact text-5xl font-bold font-poppins text-center w-max subpixel-antialiased mx-auto p-2 pt-20'>Contact</h2>
+      <h2 className='header-contact text-5xl font-bold font-poppins text-center w-max subpixel-antialiased mx-auto p-2 pt-36'>Contact</h2>
       <h3 className='text-2xl font-bold mb-5 text-white mt-32'>Let's get in touch!</h3>
        <div className="mt-20 mb-10 px-3 flex justify-center gap-6 ">
           <a href='mailto:dante.nkaddarian@gmail.com'>
